1574a564f9b9c875096012a11ffb9b0590cff03a
Arezki SABA <Arezki.SABA@prevoir.com>
2024-06-28 00:09:48 +0200

    Add loader

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -2,16 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 const HomeComponent = () => {
     const [output, setOutput] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleRunCommand = () => {
+        setIsLoading(true);
         window.electron.runPowerShellCommand('Get-AppxPackage');
     };
 
     useEffect(() => {
         window.electron.onPowerShellCommandResult((result) => {
             setOutput(result);
+            setIsLoading(false);
         });
     }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
